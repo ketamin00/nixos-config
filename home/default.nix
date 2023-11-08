2ca326b2bd9f86259933927cd06a7f3e2dc0e8ee
ketamin00 <julius.rantanen@hotmail.com>
2023-11-08 04:01:25 +0100

    renamed home.nix

--- home/default.nix
+++ home/default.nix
@@ -1,7 +1,62 @@
-{ ... }:
+{ config, pkgs, ... }:
 {
+  home.stateVersion = "23.05";
+  home.username = "julius";
+  home.homeDirectory = "/home/julius";
+  programs.home-manager.enable = true;
+
+
+  home.packages = with pkgs; [
+    firefox
+    neofetch
+    wl-clipboard
+    xdg-utils
+    zip
+    xz
+    unzip
+    p7zip
+    ripgrep
+    jq
+    dnsutils
+    file
+    which
+    tree
+    bottom
+    grim
+    slurp
+    pavucontrol
+    blueman
+    swaybg
+
+    shared-mime-info
+    lxmenu-data
+
+    gruvbox-gtk-theme
+    simp1e-cursors
+    gruvbox-dark-icons-gtk
+
+    signal-desktop
+    pcmanfm
+
+    spotify
+    (nerdfonts.override {fonts = [ "JetBrainsMono" "FiraCode" ];})
+  ];
+  fonts.fontconfig.enable = true;
+
+  programs.git = {
+    enable = true;
+    userName = "ketamin00";
+    userEmail = "julius.rantanen@hotmail.com";
+  };
 
   imports = [
-    ./home.nix
+    ./sway.nix
+    ./wofi.nix
+    ./waybar.nix
+    ./gtk.nix
+    ./vscode.nix
+    ./playerctl.nix
+    ./terminal
   ];
+
 }
