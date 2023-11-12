5d8c1ceb233e52e1842d71392c230f7a272fb41c
ketamin00 <julius.rantanen@hotmail.com>
2023-11-12 23:59:40 +0100

    conky default config

--- home/desktop/conky-module/module.nix
+++ home/desktop/conky-module/module.nix
@@ -4,6 +4,12 @@
   options.programs.conky = {
     enable = lib.mkEnableOption "Conky desktop widget";
 
+    config = lib.mkOption {
+      type = lib.types.str;
+      default = builtins.readFile ./default.conf;
+      description = "Conky configuration.";
+    };
+
   };
 
   config = lib.mkIf config.programs.conky.enable {
