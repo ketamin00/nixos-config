41d7fb85e08a3d3eabb39750b6fcc30c1d4bcf40
ketamin00 <julius.rantanen@hotmail.com>
2023-11-13 01:57:31 +0100

    new solution

--- home/shell/astronvim-module/package.nix
+++ home/shell/astronvim-module/package.nix
@@ -5,9 +5,12 @@ stdenv.mkDerivation {
   src = astronvim;
   buildInputs = [ pkgs.neovim ];
 
+  
   installPhase = ''
+    export XDG_CONFIG_HOME=$TMPDIR
+    mv $src $XDG_CONFIG_HOME/nvim/
     nvim --headless -c 'quitall'
-    mv AstroNvim/* $out
+    mv $XDG_CONFIG_HOME/nvim/ $out
   '';
 
 }
