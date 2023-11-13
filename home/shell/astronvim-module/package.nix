4c5e1aa1797fd25bb5f160b6d72a1e5de7019cf3
ketamin00 <julius.rantanen@hotmail.com>
2023-11-13 01:35:10 +0100

    adding neovim as buildinput

--- home/shell/astronvim-module/package.nix
+++ home/shell/astronvim-module/package.nix
@@ -1,8 +1,9 @@
-{ stdenv, astronvim, ...}:
+{ stdenv, pkgs, astronvim, ...}:
 
 stdenv.mkDerivation {
   name = "astronvim";
   src = astronvim;
+  buildInputs = [ pkgs.neovim ];
 
   installPhase = ''
     nvim --headless -c 'quitall'
