37dac664ed23026c0547e019c76403f91168bf7e
ketamin00 <julius.rantanen@hotmail.com>
2024-01-10 19:14:08 +0100

    added more sync between legacy features

--- hosts/components/nix.nix
+++ hosts/components/nix.nix
@@ -1,9 +1,14 @@
-{ inputs, outputs, lib, config, pkgs, ... }:
-
 {
+  inputs,
+  outputs,
+  lib,
+  config,
+  pkgs,
+  ...
+}: {
   nix = {
     settings = {
-      experimental-features = [ "nix-command" "flakes" ];
+      experimental-features = ["nix-command" "flakes"];
       auto-optimise-store = true;
     };
     gc = {
@@ -12,7 +17,17 @@
       options = "--delete-older-than 1w";
     };
     registry = (lib.mapAttrs (_: flake: {inherit flake;})) ((lib.filterAttrs (_: lib.isType "flake")) inputs);
+    nixPath = ["etc/nix/path"];
   };
+
+  environment.etc =
+    lib.mapAttrs'
+    (name: value: {
+      name = "nix/path/${name}";
+      value.source = value.flake;
+    })
+    config.nix.registry;
+
   nixpkgs = {
     overlays = [
       outputs.overlays.additions
