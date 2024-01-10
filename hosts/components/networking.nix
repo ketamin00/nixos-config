59efb300edfdab3835d0d073c0203b33a73482ff
ketamin00 <julius.rantanen@hotmail.com>
2024-01-10 18:10:24 +0100

    added local dns resolver

--- hosts/components/networking.nix
+++ hosts/components/networking.nix
@@ -6,4 +6,14 @@
 
   networking.networkmanager.enable = true;
   networking.hostName = "julius-nixos";
+
+  services.resolved = {
+    enable = true;
+#    dnssec = "allow-downgrade";
+#    domains = [ "~." ];
+#    fallbackDns = [ "1.1.1.1#one.one.one.one" "1.0.0.1#one.one.one.one" ];
+#    extraConfig = ''
+#      DNSOverTLS=yes
+#    '';
+  };
 }
