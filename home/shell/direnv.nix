141748eb3b9f7b1f82f82cd26d52ec3de3a3800a
ketamin00 <julius.rantanen@hotmail.com>
2024-02-29 21:28:00 +0100

    change direnv and nix-direnv to unstable channel

--- home/shell/direnv.nix
+++ home/shell/direnv.nix
@@ -6,9 +6,11 @@
   programs = {
     direnv = {
       enable = true;
+      package = pkgs.unstable.direnv;
       enableZshIntegration = true;
       nix-direnv = {
         enable = true;
+        package = pkgs.unstable.nix-direnv;
       };
     };
   };
